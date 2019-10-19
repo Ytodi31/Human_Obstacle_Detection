503a6a0dc62195510462385a00cc3c6a2828317b
Chinj17 <chinmaynj17@gmail.com>
2019-10-19 15:16:12 -0400

    Added the Human Detection class which will call all the other Helper classes in main.cpp

---
+++ app/HumanDetection.cpp
@@ -0,0 +1,38 @@
+/**
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License
+ *
+ * @file HumanDetection.cpp
+ * @brief HumanDetection class implementation file
+ *
+ * It contains the function defintions and members to run the HumanDetection class
+ *
+ * @author Chinmay Joshi
+ * @author Yashaarth Todi
+ * @author Sandra Tinta
+ *
+ * @date 10-18-2019
+ */
+
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <ImageProcessingHelper.h>
+#include <ImageReaderHelper.h>
+#include <HumanDetection.h>
+
+/**
+ * @brief function to call the ImageProcessingHelper class,
+ * @param none
+ * @return none
+ */
+ void HumanDetect::ImageProcessingHelperClass() {
+   ImageProcessingHelper imgProcHelp;
+   ImageReaderHelper imgReadHelp;
+   cv::Mat image = imgReadHelp.ReadCameraImg();
+   image = imgProcHelp.ResizeImg(image);
+   image = imgProcHelp.ProcessImg(image);
+   cv::imshow( "image", image);
+   cv::waitKey(0);
+   cv::destroyAllWindows();
+ }
