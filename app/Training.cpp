f5d804d004adb15b6b7eb8db713c11eddd4321d7
Chinj17 <chinmaynj17@gmail.com>
2019-10-20 04:05:20 -0400

    [ID:2.2.5]: Trained the classifier using Train data from the dataset

---
+++ app/Training.cpp
@@ -0,0 +1,81 @@
+/**
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License
+ *
+ * @file Classifier.cpp
+ * @brief Classifier class implementation file
+ *
+ * It contains the function and member implementation for classifying and detecting human
+ *
+ * @author Chinmay Joshi
+ * @author Yashaarth Todi
+ * @author Sandra Tinta
+ *
+ * @date 10-20-2019
+ */
+
+#include <Classifier.h>
+#include <Training.h>
+
+using std::cout;
+using std::endl;
+
+void Training::getTrainClassifier() {
+  ImageProcessingHelper imgProc;
+  ImageReaderHelper imgReader;
+  getHogFeatures();
+  setClassifier();
+  std::vector <cv::Mat> gradientList;
+  std::vector <float> descriptors;
+  std::vector <cv::Point> locations;
+    std::vector < int > labels;
+  cv::Mat gray;
+  cv::String posPath = "/home/ytodi31/INRIAPerson/Train/pos/*.png";
+  imgProc.RegionInterest(posPath);
+  for (auto img: imgProc.roiTraining) {
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    hog.compute(gray, descriptors, cv::Size(0, 0), cv::Size(0, 0), locations);
+    gradientList.push_back(cv::Mat(descriptors).clone());
+    labels.push_back(1);
+  }
+
+  cv::String negPath = "/home/ytodi31/INRIAPerson/Train/neg/*";
+  std::vector <cv::Mat> tempNegImages= imgReader.ReadImages(negPath);
+  std::vector <cv::Mat> negImages;
+  cv::Rect box;
+  for (auto img:tempNegImages) {
+    int imageHeight = img.rows;
+    int imageWidth = img.cols;
+    box = cv::Rect(int(imageWidth/4), int(imageHeight/4), int(imageHeight/2), int(imageHeight/2));
+    cv::Mat croppedImage = img(box);
+    croppedImage = imgProc.ReSizeImg(croppedImage);
+    negImages.push_back(croppedImage);
+  }
+  std::cout << negImages.size() << std::endl;
+  for (auto img: negImages) {
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    hog.compute(gray, descriptors, cv::Size(0, 0), cv::Size(0, 0), locations);
+    gradientList.push_back(cv::Mat(descriptors).clone());
+    labels.push_back(0);
+
+  }
+  std::cout << labels.size() << std::endl;
+
+//CHANGE GRADIENTLIST VAR
+
+  const int rows = (int)(gradientList.size());
+  const int cols = (int)(std::max(gradientList[0].cols, gradientList[0].rows));
+  cv::Mat temp(1, cols, CV_32FC1), trainData(rows, cols, CV_32FC1);
+  for (unsigned int index = 0; index < gradientList.size(); index++) {
+      if (gradientList[index].cols == 1) {
+          transpose(gradientList[index], temp);
+          temp.copyTo(trainData.row(int(index)));
+      } else if (gradientList[index].rows == 1) {
+          gradientList[index].copyTo(trainData.row(int(index)));
+      }
+  }
+  // Training Starts
+  std::cout << "Training SVM Classifier" << std::endl;
+  classifier->train(trainData, cv::ml::ROW_SAMPLE, cv::Mat(labels));
+  std::cout << "Training Finshed" << std::endl;
+}
---
+++ include/Training.h
@@ -0,0 +1,40 @@
+/**
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License
+ *
+ * @file Classifier.h
+ * @brief Classifier class header file
+ *
+ * It contains the function defintions and members for classifying and detecting human
+ *
+ * @author Chinmay Joshi
+ * @author Yashaarth Todi
+ * @author Sandra Tinta
+ *
+ * @date 10-20-2019
+ */
+
+#ifndef APP_TRAINING_HPP_
+#define APP_TRAINING_HPP_
+
+#include <string>
+#include <vector>
+#include <../include/opencv2/core/core.hpp>
+#include <../include/opencv2/opencv.hpp>
+#include <../include/opencv2/highgui/highgui.hpp>
+#include <../include/opencv2/ml.hpp>
+#include <Classifier.h>
+
+class Training: public Classifier {
+public:
+  
+  /**
+    * @brief Initializes the svm parameters
+    * @param none
+    * @return none
+    */
+  void getTrainClassifier();
+
+};
+
+#endif /* APP_TRAINING_HPP_ */
