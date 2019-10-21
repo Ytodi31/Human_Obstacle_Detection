58411d5bcc5f2efdea87423a870975e71d75b004
CJ <chinmaynj17@gmail.com>
2019-10-21 14:15:31 -0400

    [ID:2.4.2]: edited the code testTrainingClass.cpp as it was missing doxygen comments

--- test/testTrainingClass.cpp
+++ test/testTrainingClass.cpp
@@ -1,13 +1,24 @@
+/**
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License
+ *
+ * @file testImageProcessingHelper.cpp
+ * @brief Test file for ImageProcessingHelper.cpp
+ *
+ * It contains the tests for methods of ImageProcessingHelper class
+ *
+ * @author Yashaarth Todi
+ * @author Chinmay Joshi
+ * @author Sandra Tinta
+ *
+ * @date 10-20-2019
+ */
 #include <gtest/gtest.h>
 #include <../include/Training.h>
 #include <../include/Classifier.h>
 #include <../include/ImageReaderHelper.h>
 #include <../include/ImageProcessingHelper.h>
 
-
-
-
-// Unit test of class ImageReaderHelper
 
 TEST(Training, LabelsTrainingTest) {
   // Check if the images were read
@@ -26,12 +37,3 @@ TEST(Training, FeaturesTrainingTest) {
 
   ASSERT_GT(trainTest.features.size(), i);
 }
-
-// TEST(Training, DescriptorsTrainingTest) {
-//   // Check if the images were read
-//   Training trainTest;
-//   trainTest.getTrainClassifier();
-//   unsigned int i = 0;
-//
-//   ASSERT_GT(trainTest.descriptors.size(), i);
-// }
